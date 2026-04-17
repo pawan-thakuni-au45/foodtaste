de97b1e3bd608682c319f861d1100352cc8420f9
pawan <thakunipawan@gmail.com>
2026-04-17 13:44:38 +0530

    feat: initial commit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      tgtrhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+      guuu hkaefregrekngrkgnknger,klklfmklsdnfksdnfkjsdnfdsfd,
+      so basically i have two folder inside this one root folder ,frontend and backend , and i am using shadcn ui ,which is just a library for our UI
+       
+    </div>
+  );
+}
+
